b1361c8808d4b48e5f994fb49fab6cf8fe913fd5
Simas <simas.raudonis@gmail.com>
2021-07-19 15:45:29 +0200

    enabled vue history mode

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   transpileDependencies: ["vuetify", "vuex-persist"],
   publicPath:
-    process.env.NODE_ENV === "production" ? "/Internet-Radio-Player-Vue" : "/",
+  process.env.NODE_ENV === "production" ? "/Internet-Radio-Player-Vue" : "/",
 };
